9798e4cd83517530a83a0b96a5ec16634f202dae
m.vanhoof <m.vanhoof@youwe.nl>
2019-04-20 17:28:13 +0200

    rename analysis to make_tokens

--- make_tokens.py
+++ make_tokens.py
@@ -1,38 +1,41 @@
+def make_tokens()
+
+
 def analyse_word(word):
     print(repr(word))
-    dest_html_word = word
-    return dest_html_word
+    dest_token_word = word
+    return dest_token_word
     pass
 
 
 def analyse_line(line):
-    dest_html_line = ''
+    dest_token_line = ''
 
     line = line.rstrip("\n\r")
     line = line.split(" ")
     for word in line:
         analysed_word = analyse_word(word)
 
-        dest_html_line += analysed_word
+        dest_token_line += analysed_word
 
-    return dest_html_line
+    return dest_token_line
 
 
 def loop_through_source(source_file):
-    dest_html = ''
+    dest_token = ''
     previous_line = ''
     for line in source_file:
-        dest_html_line = ''
+        dest_token_line = ''
         analysed_line = analyse_line(line)
         previous_line = line
 
-        dest_html_line += analysed_line
+        dest_token_line += analysed_line
 
-        dest_html += dest_html_line
+        dest_token += dest_token_line
 
-    return dest_html
+    return dest_token
 
 
 def analyse_source(source_file):
-    dest_html = loop_through_source(source_file)
-    return dest_html
+    dest_token = loop_through_source(source_file)
+    return dest_token
