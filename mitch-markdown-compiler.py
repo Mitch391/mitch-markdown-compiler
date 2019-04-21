5facbd99c3688ab1634b981dfdddd539931b1e8a
m.vanhoof <m.vanhoof@youwe.nl>
2019-04-21 17:27:52 +0200

    make more standard

--- mitch-markdown-compiler.py
+++ mitch-markdown-compiler.py
@@ -7,18 +7,18 @@ import make_nodes
 import keep_track_of_nodes
 import nodes
 
-def exists_output_file(file_name_html):
-    if os.path.exists(file_name_html):
+def exists_output_file(output_file_name):
+    if os.path.exists(output_file_name):
         text = "Are you sure you want to overwrite "
-        text += file_name_html
+        text += output_file_name
         text += "? (y/n)"
         answer = input(text)
         if not (answer == "y" or answer == ''):
             exit(1)
-        os.remove(file_name_html)
+        os.remove(output_file_name)
 
 
-def output_file_standard_head_text(file_name):
+def output_file_html_standard_head_text(file_name):
     text = "<!DOCTYPE html>\n"
     text += "<html>\n"
     text += "<head>\n"
@@ -27,13 +27,13 @@ def output_file_standard_head_text(file_name):
     return text
 
 
-def output_file_standard_bottom_text():
+def output_file_html_standard_bottom_text():
     text = "</body>\n"
     text += "</html>"
     return text
 
 
-def make_output_file(file_name, file_type='html'):
+def make_output_file_html(file_name, file_type='html'):
     file_name_html = file_name.rstrip(".md")
     file_name_html = file_name_html + ".html"
     exists_output_file(file_name_html)
@@ -41,15 +41,14 @@ def make_output_file(file_name, file_type='html'):
     return dest_file
 
     
-
 if __name__ == '__main__':
     if (sys.argv[0] == sys.argv[-1]):
         print("Please add a file to compile")
 
     file_name, source_file, flags = checks.check_all(sys.argv)
 
-    dest_file = make_output_file(file_name)
-    standard_head_text = output_file_standard_head_text(file_name)
+    dest_file = make_output_file_html(file_name)
+    standard_head_text = output_file_html_standard_head_text(file_name)
     dest_file.write(standard_head_text)
 
     track_node = keep_track_of_nodes.Keep_track_of_nodes()
@@ -59,6 +58,6 @@ if __name__ == '__main__':
     html = make_nodes.analyse_source(source_file, track_node)
     dest_file.write(html)
     
-    standard_bottom_text = output_file_standard_bottom_text()
+    standard_bottom_text = output_file_html_standard_bottom_text()
     dest_file.write(standard_bottom_text)
     start_node.visualize_node_stream()
