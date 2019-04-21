fc9e7b3e8f06502ede8dc207ff49b7499e339cbf
m.vanhoof <m.vanhoof@youwe.nl>
2019-04-21 16:58:26 +0200

    separate functions

--- make_nodes.py
+++ make_nodes.py
@@ -2,18 +2,28 @@ import re
 import keep_track_of_nodes
 import nodes
 
+def open_node(curr_node, type_next_node, text, found_span):
+    prev_node = curr_node
+    prev_node.set_text(text[:found_span[0]])
+    curr_node = nodes.Node("bold", [text[found_span[1]:]], prev_node, [])
+    prev_node.add_next_node(curr_node)
+
+    return prev_node, curr_node
+
+def close_node(curr_node, text, found_span):
+    curr_node.set_text(text[:found_span[0]])
+    curr_node = curr_node.prev_node
+    curr_node.add_text(text[found_span[1]:])
+
+    return curr_node
+
 def search_for_markup(text, track_node, curr_node, pointer, found):
     found_bold = track_node.check_for_bold_in_text(text)
     if found_bold and track_node.bold == True:
-        prev_node = curr_node
-        prev_node.set_text(text[:found_bold[0]])
-        curr_node = nodes.Node("bold", [text[found_bold[1]:]], prev_node, [])
-        prev_node.add_next_node(curr_node)
+        prev_node, curr_node = open_node(curr_node, "bold", text, found_bold)
         found = True
     elif found_bold and track_node.bold == False:
-        curr_node.set_text(text[:found_bold[0]])
-        curr_node = curr_node.prev_node
-        curr_node.add_text(text[found_bold[1]:])
+        curr_node = close_node(curr_node, text, found_bold)
         found = True
 
     return found, curr_node
