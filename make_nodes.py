0276f9dae56ba4ea6600ec5c7a71a6353f6befe5
m.vanhoof <m.vanhoof@youwe.nl>
2019-04-21 16:50:37 +0200

    remove depricated code

--- make_nodes.py
+++ make_nodes.py
@@ -2,60 +2,6 @@ import re
 import keep_track_of_nodes
 import nodes
 
-
-def analyse_word(word, track_node, node):
-    if track_node.check_for_bold_begin(word):
-        pass
-    dest_node_word = word
-    return dest_node_word
-
-
-def analyse_line(line, track_node, curr_node):
-    dest_node_line = []
-
-    if track_node.check_for_horizontal_rule(line):
-        node_text = line.strip()
-        prev_node = curr_node
-        curr_node = nodes.Node("horizontal_rule", node_text, prev_node)
-        prev_node.next_node = curr_node
-
-    stripped_line = line.rstrip("\n\r")
-    split_line = stripped_line.split(" ")
-    for word in split_line:
-        analysed_word = analyse_word(word, track_node, curr_node)
-
-        dest_node_line.append(analysed_word)
-
-
-    if track_node.check_for_linebreak(line):
-        node_text = line.strip()
-        prev_node = curr_node
-        curr_node = nodes.Node("linebreak", node_text, prev_node)
-        prev_node.next_node = curr_node
-
-    dest_node_line.append("\n")
-
-    dest_html = " ".join(dest_node_line).strip(" ")
-
-    return curr_node, dest_html
-
-
-def make_nodes(source_file, track_node, start_node):
-    dest_nodes = ''
-    previous_line = ''
-    curr_node = start_node
-    for line in source_file:
-        dest_node_line = ''
-        curr_node, analysed_line = analyse_line(line, track_node, curr_node)
-        previous_line = line
-
-        dest_node_line += analysed_line
-
-        dest_nodes += dest_node_line
-
-    return dest_nodes
-
-
 def search_for_markup(text, track_node, curr_node, pointer, found):
     found_bold = track_node.check_for_bold_in_text(text)
     if found_bold and track_node.bold == True:
